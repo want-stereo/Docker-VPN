98e676229ed780ffe8273afec9cf278a15064e3b
moriah das <wand-stereo-twisty@duck.com>
2026-02-27 14:24:24 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Replace placeholders with environment variables
 sed -i "s/\${UUID}/${UUID}/g" /etc/xray/config.json
 sed -i "s/\${WSPATH}/${WSPATH}/g" /etc/xray/config.json
+# Start xray
 xray -config /etc/xray/config.json
